6fee93a0c970e2200576eee98b9b7b233a333785
ik012982i9 <hdiazdemendivilvi23dw@ikzubirimanteo.com>
2024-01-10 14:19:42 +0100

    Hacer Vagon y Tren

---
+++ src/Tren.java
@@ -0,0 +1,16 @@
+public class Tren {
+    public int codigo_identificacion;
+    public String jefe;
+    public  String MaquinaTractora;
+    public String Vagon;
+
+
+    public Tren (int codigo_identificacion, String jefe, String MaquinaTractora, String Vagon){
+        this.codigo_identificacion=codigo_identificacion;
+        this.jefe=jefe;
+        this.MaquinaTractora=MaquinaTractora;
+        this.Vagon=Vagon;
+    }
+
+}
+
---
+++ src/Vagon.java
@@ -0,0 +1,7 @@
+public class Vagon{
+    public int capacidad_max;
+
+    public Vagon{
+        this.capacidad_max=capacidad_max;
+    }
+}
